297caa148078d0cb428d8d75bd5c82d5f90d5504
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2021-12-21 16:53:12 -0800

    clean up navigation, css effect

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,6 @@ fetch(
   }
 ).then(function (response) {
   response.json().then(function (webcam) {
-    console.log(webcam);
     changeBackground(webcam);
   });
 });
@@ -38,19 +37,3 @@ changeBackground = function (webcam) {
   container.appendChild(title);
 };
 
-// //parallax
-// window.addEventListener("scroll", function (event) {
-//   var topDistance = window.scrollY;
-//   layers = document.querySelectorAll("[data-type='parallax']");
-
-//   for (var i = 0; i < layers.length; i++) {
-//     depth = layers[i].getAttribute("data-depth");
-//     movement = (topDistance * -1) * depth;
-//     translate3d = "translate3d(0, " + movement + "px, 0)";
-//     // layers[i].style["-webkit-transform"] = translate3d;
-//     // layers[i].style["-moz-transform"] = translate3d;
-//     // layers[i].style["-ms-transform"] = translate3d;
-//     // layers[i].style["-o-transform"] = translate3d;
-//     layers[i].style.transform = translate3d;
-//   }
-// });
