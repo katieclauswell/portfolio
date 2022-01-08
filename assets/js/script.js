99241dc2f58974e166f09003c5ca3106f4c8b6d2
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-01-08 11:48:40 -0800

    transparency

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,22 +27,21 @@ fetch(
 });
 
 changeBackground = function (webcam) {
-  //generate DOM elements
-  var container = document.getElementById("landing-page");
-
   //random index
   var max = webcam.result.webcams.length;
   var i = Math.floor(Math.random() * max);
 
-  //get random img
+  //set background
+  var container = document.getElementById("landing-page");
   var imgSrc = webcam.result.webcams[i].image.current.preview;
   container.style.backgroundImage = "url('" + imgSrc + "')"
 
   //titling img
+  var containerText = document.getElementById("landing-page-text");
   var imgTitle = webcam.result.webcams[i].title;
   var title = document.createElement("div");
   title.setAttribute("id", "webcam-title");
-  title.innerHTML = "<p>The background is a randomly generated live image of the mountains in Oregon. <br> This one happens to be in "  + imgTitle + ".</p>"
-  container.appendChild(title);
+  title.innerHTML = "<p>Randomly generated live background of the mountains in Oregon. <br> This one happens to be in "  + imgTitle + ".</p>"
+  containerText.appendChild(title);
 };
 
