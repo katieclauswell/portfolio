24c743cc04ae4f6112038d80e73fc9fffd0d7e45
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-01-11 18:55:17 -0800

    dither.js tests

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,47 +12,54 @@
 // }
 
 //webcam API
-fetch(
-  "https://api.windy.com/api/webcams/v2/list/category=mountain/region=US.OR?show=webcams:image",
-  {
-    method: "GET",
-    headers: {
-      "x-windy-key": "fRBdejZXV9tukKAYkPngOtGOg0bYrvV9",
-    },
-  }
-).then(function (response) {
-  response.json().then(function (webcam) {
-    changeBackground(webcam);
-  });
-});
+// fetch(
+//   "https://api.windy.com/api/webcams/v2/list/category=mountain/region=US.OR?show=webcams:image",
+//   {
+//     method: "GET",
+//     headers: {
+//       "x-windy-key": "fRBdejZXV9tukKAYkPngOtGOg0bYrvV9",
+//     },
+//   }
+// ).then(function (response) {
+//   response.json().then(function (webcam) {
+//     changeBackground(webcam);
+//   });
+// });
 
 changeBackground = function (webcam) {
   //random index
   var max = webcam.result.webcams.length;
   var i = Math.floor(Math.random() * max);
+  var image = document.createElement("img");
 
   //set background
   var container = document.getElementById("landing-page");
-  var imgSrc = webcam.result.webcams[i].image.current.preview;
+  // var imgSrc = webcam.result.webcams[i].image.current.preview;
 
-  // const imgSrc = 'assets/images/dither_it_1333319444.jpeg' //dither test img
+  //dither test
+  // container.appendChild(image);
+  // image.setAttribute("class", "dithered-image");
+  // image.setAttribute("onload", "ditherjs.dither(this)")
+  // image.setAttribute( "src", 'assets/images/test-image.jpeg')
+
+  // var options = {
+  //   step: 1, // The step for the pixel quantization n = 1,2,3...
+  //   // palette: defaultPalette, // an array of colors as rgb arrays
+  //   algorithm: "ordered", // one of ["ordered", "diffusion", "atkinson"]
+  // };
+
+  // const imgSrc = 'assets/images/dither-test.jpeg' //dither test img
   // container.style.backgroundImage = "url('" + imgSrc + "')"; //original webcam
   // container.style.backgroundImage = "url('" + ImgSrc + "')";
 
-  var img = new Image();
-  img.onload = function () {
-    eightBit(document.getElementById("mycanvas"), img, 70);
-  };
-  img.src = imgSrc;
-
   //titling img
-  var containerText = document.getElementById("landing-page-text");
-  var imgTitle = webcam.result.webcams[i].title;
-  var title = document.createElement("div");
-  title.setAttribute("id", "webcam-title");
-  title.innerHTML =
-    "<p>Randomly generated live background of the mountains in Oregon. <br> This one happens to be in " +
-    imgTitle +
-    ".</p>";
-  containerText.appendChild(title);
+  // var containerText = document.getElementById("landing-page-text");
+  // var imgTitle = webcam.result.webcams[i].title;
+  // var title = document.createElement("div");
+  // title.setAttribute("id", "webcam-title");
+  // title.innerHTML =
+  //   "<p>Randomly generated live background of the mountains in Oregon. <br> This one happens to be in " +
+  //   imgTitle +
+  //   ".</p>";
+  // containerText.appendChild(title);
 };
