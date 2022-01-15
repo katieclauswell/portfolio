4a26d5a9d65b364f08f7319763f5d0ad6c911770
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-01-15 14:51:24 -0800

    draft layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ randomImage = function (webcam) {
   var title = document.createElement("div");
   title.setAttribute("id", "webcam-title");
   title.innerHTML =
-    "<p>Randomly generated live background of the mountains in Oregon. <br> This one happens to be in " +
+    "<p>< Randomly generated live background of the mountains in Oregon. This one happens to be in " +
     imgTitle +
     ".</p>";
   containerText.appendChild(title);
