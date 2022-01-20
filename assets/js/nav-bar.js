31874c599246410ba8720fda8432b5b25226fb4d
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-01-19 21:25:38 -0800

    sticky, scales correctly

--- assets/js/nav-bar.js
+++ assets/js/nav-bar.js
@@ -2,29 +2,29 @@ var container = document.getElementById("landing-page");
 var canvas = document.getElementsByTagName("canvas");
 var nav = document.getElementById("landing-page-text");
 var intro = document.getElementById("intro");
-
+var webcamTitle = document.getElementById("webcam-title");
 
 window.onscroll = function () {
+  canvas.style.transition = "0.4s";
   scrollFunction();
 };
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    // canvas.style.display = "none";
-    // intro.style.display = "none";
-    nav.style.height = "10vh";
-    nav.style.width = "100vw";
+    //remove canvas
+    canvas.style.display = "none";
+    //container
     container.style.height = "fit-content";
-    container.style.
-    // container.style.display = "block";
-    container.innerHTML = `
-    <nav>
+    container.style.position = "sticky";
+    container.style.top = "0";
+    //text
+    nav.style.width = "100vw";
+    nav.style.height = "fit-content";
+    nav.innerHTML = `<nav>
     <ul>
-      <li><a href="#about"> About & Contact</a></li>
-      <li><a href="#portfolio"> Portfolio</a></li>
+      <li><a href="#about">> About & Contact</a></li>
+      <li><a href="#portfolio">> Portfolio</a></li>
       <!-- <li>> Resume</li> -->
-    </ul>
-  </nav>
-    `;
-  }
+    </ul>`;
+  } //else
 }
