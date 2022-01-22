e5cd8cacbc7ab7422aedd668354689d9b19882a9
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-01-21 20:22:29 -0800

    bug, unhighlight one at a time and no rehighlight

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,22 @@
-const languageToggle = document.getElementById("switch");
+const languageToggle = document.getElementById("switch-click");
 let cardBody = document.getElementsByClassName("text");
 
 const languages = /HTML|JavaScript|JQuery|CSS|API/gi;
 
-// languageToggle.addEventListener("click", highlight);
-
-// var highlight = function () {
-for (i = 0; i < cardBody.length; i++) {
-  var cardText = cardBody[i].innerHTML;
-  console.log(cardBody[i].innerHTML)
-  var res = cardText.replace(languages, function (matched) {
-    return "<span class='html'>" + matched + "</span>";
-  });
-  cardBody[i].innerHTML = res;
-}
-// };
+languageToggle.addEventListener("click", function () {
+  for (i = 0; i < cardBody.length; i++) {
+    var cardText = cardBody[i].innerHTML;
+    if (cardText.includes("<span class=")) {
+      var reset = cardText.replace(' class="html"', "");
+      cardBody[i].innerHTML = reset;
+    } else if (cardText.includes("<span>")) {
+      var add = cardText.replace("<span>, '<span class='html'>");
+      cardBody[i].innerHTML = add;
+    } else {
+      var start = cardText.replace(languages, function (matched) {
+        return "<span class='html'>" + matched + "</span>";
+      });
+      cardBody[i].innerHTML = start;
+    }
+  }
+});
