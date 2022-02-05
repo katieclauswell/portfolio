3134793030b9786e7ad0eeec8e9422a70e9ffcf8
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-02-04 20:22:50 -0800

    box shadow

--- assets/js/webcam-api.js
+++ assets/js/webcam-api.js
@@ -25,7 +25,7 @@ function randomImage(webcam) {
   var title = document.createElement("div");
   title.setAttribute("id", "webcam-title");
   title.innerHTML =
-    "<p>Randomly generated live image of the mountains of Oregon --</p><p>This happens to be in " +
+    "<p>Randomly generated live image of the mountains of Oregon --</p><p>This one happens to be in " +
     imgTitle +
     ".</p>";
   containerText.appendChild(title);
