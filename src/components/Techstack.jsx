d49e62fd2074898e188bc3a9619a4aab579cf4cc
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-08-10 04:46:28 -0700

    Removed techstack popover

--- src/components/Techstack.jsx
+++ src/components/Techstack.jsx
@@ -1,81 +1,23 @@
-import React, { useState, useEffect, useRef } from "react";
-import { Container, Row, Col, Popover, OverlayTrigger } from "react-bootstrap";
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import { info } from "../info/info";
-import LoadingSpinner from "../components/LoadingSpinner";
-import PopoverDesc from "./PopoverDesc";
-import axios from "axios";
 
 function Techstack() {
-  // Github Repositories
-  const [repos, setRepos] = useState();
-  // Popover
-  const [show, setShow] = useState(false);
-  const target = useRef(null);
-  // Loadig
-  const [isLoading, setIsLoading] = useState();
-
-  // get repo data
-  useEffect(() => {
-    setIsLoading(true);
-    const fetchTopics = async () => {
-      const options = {
-        method: "GET",
-        url:
-          process.env.NODE_ENV === "production"
-            ? "/github"
-            : "http://localhost:8000/github",
-      };
-
-      axios
-        .request(options)
-        .then(async (response) => {
-          const data = await response;
-          setRepos(data.data);
-          setIsLoading(false);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    };
-    fetchTopics();
-  }, []);
-
   return (
     <Container>
       <Row className="m-3">
         <h2>Tech Stack</h2>
-        <p>Click an icon to filter my GitHub repositories by that topic.</p>
       </Row>
       <Row xs={2} md={4} lg={6} className="m-1">
         {info.technologies.map((item, index) => (
           <>
             <Col className="d-flex justify-content-center" key={index}>
-              <OverlayTrigger
-                trigger="click"
-                key={index}
-                placement="right"
-                overlay={
-                  <Popover id={`popover-positioned-right`}>
-                    <Popover.Header as="h3">{`${item.name}`}</Popover.Header>
-                    <Popover.Body>
-                      {isLoading === true ? (
-                        <LoadingSpinner />
-                      ) : (
-                        <PopoverDesc language={item.name} repos={repos} />
-                      )}
-                    </Popover.Body>
-                  </Popover>
-                }
-              >
                 <div
-                  ref={target}
                   className="m-1 justify-content-md-center"
-                  onClick={() => setShow(!show)}
                 >
                   <i className={item.icon}></i>
                   <div className="text-center">{item.name}</div>
                 </div>
-              </OverlayTrigger>
             </Col>
           </>
         ))}
