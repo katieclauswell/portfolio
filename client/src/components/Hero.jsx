aa091dfa6255d7bcbaaff6d3d7b7dc6541ab744e
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-09-14 14:48:18 -0700

    removed webcam api call, shuffle through array instead

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -6,11 +6,13 @@ import axios from "axios";
 import windowUrl from "../assets/images/window-2.png";
 
 function Hero() {
-  const [isLoading, setIsLoading] = useState(false);
   const [webcam, setWebcam] = useState();
 
   // set webcam image height and width equal to window image
-  const [windowDimensions, setWindowDimensions] = useState();
+  const [windowDimensions, setWindowDimensions] = useState({
+    height: "100",
+    width: "100",
+  });
 
   const handleImageLoad = (event) => {
     setWindowDimensions({
@@ -19,66 +21,42 @@ function Hero() {
     });
   };
 
-  // fetch webcam data from api
+  // choose random webcam image
   useEffect(() => {
-    setIsLoading(true);
-    const fetchWebcam = async () => {
-      const options = {
-        method: "GET",
-        url: "https://katie-churchwell.herokuapp.com/webcam",
-      };
-      axios
-        .request(options)
-        .then(async (response) => {
-          setWebcam(randomWebcam(response.data));
-          setIsLoading(false);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    };
-    fetchWebcam();
+    setWebcam(randomWebcam());
   }, []);
 
   return (
     <Container className="m-3">
-      {isLoading === true ? (
-        <LoadingSpinner />
-      ) : (
-        <>
-          <Row>
-            <Col>
-              <h1>Katie Churchwell</h1>
-            </Col>
-          </Row>
-          <img onLoad={handleImageLoad} src={windowUrl} id="window" />
-          <Row>
-            <div
-              id="webcam"
-              style={{
-                backgroundImage: webcam
-                  ? `url(${webcam.image.current.preview})`
-                  : "none",
-                backgroundPosition: "center",
-                backgroundSize: "cover",
-                backgroundRepeat: "no-repeat",
-                height: `${windowDimensions.height}px`,
-                width: `${windowDimensions.width}px`,
-              }}
-            />
-          </Row>
-          <Row>
-            {webcam ? (
-              <p className="m-2">
-                Randomly generated live image of the mountains of Oregon -- this
-                one happens to be in {webcam.title}.
-              </p>
-            ) : (
-              <p>Hello!</p>
-            )}
-          </Row>
-        </>
-      )}
+      <Row>
+        <Col>
+          <h1>Katie Churchwell</h1>
+        </Col>
+      </Row>
+      <img onLoad={handleImageLoad} src={windowUrl} id="window" />
+      <Row>
+        <div
+          id="webcam"
+          style={{
+            backgroundImage: webcam ? `url(${webcam.image})` : "none",
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            height: `${windowDimensions.height}px`,
+            width: `${windowDimensions.width}px`,
+          }}
+        />
+      </Row>
+      <Row>
+        {webcam ? (
+          <p className="m-2">
+            Randomly generated live image of the mountains of Oregon -- this one
+            happens to be in {webcam.title}.
+          </p>
+        ) : (
+          <p>Hello!</p>
+        )}
+      </Row>
     </Container>
   );
 }
--- client/src/utils/randomWebcam.js
+++ client/src/utils/randomWebcam.js
@@ -1,11 +1,25 @@
-export default function randomWebcamImage(webcams) {
-  // Exclude Redmond (id=1614363511)
-  var redmondIndex = webcams.findIndex(
-    (webcams) => webcams.id === "1614363511"
-  );
-  webcams.splice(redmondIndex, 1);
+export default function randomWebcamImage() {
+  const webcams = [
+    {
+      title: "Sisters › West",
+      image:
+        "https://images-webcams.windy.com/74/1582164074/current/preview/1582164074.jpg",
+    },
 
-  // Random selection
+    {
+      title:
+        "Klamath › North-East: Llao Rock - Wizard Island Boat Tour Dock - Mount Thielsen",
+      image:
+        "https://images-webcams.windy.com/65/1605569565/current/preview/1605569565.jpg",
+    },
+
+    {
+      title: "Culver › North-West",
+      image:
+        "https://images-webcams.windy.com/07/1637286107/current/preview/1637286107.jpg",
+    },
+  ];
+
   var max = webcams.length;
   var i = Math.floor(Math.random() * max);
   return webcams[i];
