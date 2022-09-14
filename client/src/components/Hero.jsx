b7c1da0759434df461791d880aed00f040f2ba03
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-09-14 14:05:48 -0700

    dynamically set webcam height and width equal to window

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -3,10 +3,21 @@ import { Container, Row, Col } from "react-bootstrap";
 import randomWebcam from "../utils/randomWebcam";
 import LoadingSpinner from "./LoadingSpinner";
 import axios from "axios";
+import windowUrl from "../assets/images/window-2.png";
 
 function Hero() {
   const [isLoading, setIsLoading] = useState(false);
   const [webcam, setWebcam] = useState();
+
+  // set webcam image height and width equal to window image
+  const [windowDimensions, setWindowDimensions] = useState();
+
+  const handleImageLoad = (event) => {
+    setWindowDimensions({
+      height: event.target.clientHeight,
+      width: event.target.clientWidth,
+    });
+  };
 
   // fetch webcam data from api
   useEffect(() => {
@@ -36,6 +47,12 @@ function Hero() {
       ) : (
         <>
           <Row>
+            <Col>
+              <h1>Katie Churchwell</h1>
+            </Col>
+          </Row>
+          <img onLoad={handleImageLoad} src={windowUrl} id="window" />
+          <Row>
             <div
               id="webcam"
               style={{
@@ -45,21 +62,20 @@ function Hero() {
                 backgroundPosition: "center",
                 backgroundSize: "cover",
                 backgroundRepeat: "no-repeat",
+                height: `${windowDimensions.height}px`,
+                width: `${windowDimensions.width}px`,
               }}
             />
-            <Col>
-              <h1>Katie Churchwell</h1>
-            </Col>
-            <Col>
-              {webcam ? (
-                <p className="m-2">
-                  Randomly generated live image of the mountains of Oregon --
-                  this one happens to be in {webcam.title}.
-                </p>
-              ) : (
-                <p>Hello!</p>
-              )}
-            </Col>
+          </Row>
+          <Row>
+            {webcam ? (
+              <p className="m-2">
+                Randomly generated live image of the mountains of Oregon -- this
+                one happens to be in {webcam.title}.
+              </p>
+            ) : (
+              <p>Hello!</p>
+            )}
           </Row>
         </>
       )}
