c0d75c8857ed5fd2de6f54c73f77d58373814fdd
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-09-14 07:58:06 -0700

    404 page

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Techstack from "./components/Techstack";
 import Work from "./components/Work";
+import NotFound from "./components/NotFound";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -17,6 +18,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/tech-stack" element={<Techstack />} />
         <Route path="/work" element={<Work />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
---
+++ client/src/components/NotFound.jsx
@@ -0,0 +1,20 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+function NotFound() {
+  return (
+    <Container fluid>
+      <Row className="p-5">
+        <Col className="text-center">
+          <Row>
+            <i className="bi bi-emoji-frown"></i>
+          </Row>
+          <Row>
+            <h1>404 Not Found</h1>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default NotFound;
