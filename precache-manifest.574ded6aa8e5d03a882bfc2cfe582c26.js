1a15a88bbe0e3c0400f68c3b7d69640a7d1f8f2e
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2023-10-28 18:22:22 -0700

    Updates

---
+++ precache-manifest.574ded6aa8e5d03a882bfc2cfe582c26.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61cf68ec3619ede5ede7bf5e5aeb81c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db505c101e521692f4e4",
+    "url": "/static/css/2.4f03da6b.chunk.css"
+  },
+  {
+    "revision": "cfe34bc7408fdce197d7",
+    "url": "/static/css/main.23a1d7e2.chunk.css"
+  },
+  {
+    "revision": "db505c101e521692f4e4",
+    "url": "/static/js/2.9874f4c3.chunk.js"
+  },
+  {
+    "revision": "739c4d87a8ce3678d36a3b6b2f65362e",
+    "url": "/static/js/2.9874f4c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfe34bc7408fdce197d7",
+    "url": "/static/js/main.f33a09b6.chunk.js"
+  },
+  {
+    "revision": "f772171b3ad14fa29da0",
+    "url": "/static/js/runtime-main.9581f308.js"
+  },
+  {
+    "revision": "9915d6eb84514d8dcd5f26d46dea1abc",
+    "url": "/static/media/Window.9915d6eb.gif"
+  },
+  {
+    "revision": "28dcaa663b0fb8361f4db5668ab67f61",
+    "url": "/static/media/bliss-wallpaper.28dcaa66.jpg"
+  },
+  {
+    "revision": "a8c9ff70c29685bc5788932162fd06cd",
+    "url": "/static/media/blue-noise.a8c9ff70.png"
+  },
+  {
+    "revision": "1a51dfbe5467607389d603c02a77cd10",
+    "url": "/static/media/bootstrap-icons.1a51dfbe.woff"
+  },
+  {
+    "revision": "ed62b9f1e0c75121f4d797a4a85730a2",
+    "url": "/static/media/bootstrap-icons.ed62b9f1.woff2"
+  },
+  {
+    "revision": "a92a7664aa030dd47a54980d7c8afc8d",
+    "url": "/static/media/chewview.a92a7664.png"
+  },
+  {
+    "revision": "6fda456dcde6ddfe0982eb6f161d834c",
+    "url": "/static/media/headshot-transparent.6fda456d.png"
+  },
+  {
+    "revision": "1a1abac343de6185d05bc1b27acdd295",
+    "url": "/static/media/nasteroids.1a1abac3.png"
+  },
+  {
+    "revision": "534ddf6425c02116d8322dd026a8399b",
+    "url": "/static/media/pantry.534ddf64.png"
+  }
+]);
