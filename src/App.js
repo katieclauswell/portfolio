a6d3668f5f597939490549150671fbc92d9590f9
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2024-06-01 16:28:11 -0700

    determine mobile view with react-device-detect, format css file, MobileView component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { isMobile } from "react-device-detect";
 
 import "./assets/styles/default.css";
 import "./assets/styles/dark-mode.css";
@@ -11,23 +12,32 @@ import TechStack from "./components/TechStack/TechStack";
 import Work from "./components/Work";
 import NotFound from "./components/Misc/NotFound";
 import Resume from "./components/Resume";
+import MobileView from "./components/MobileView";
 
 function App() {
-  return (
-    <>
-      <Router>
-        <Header />
-        <Routes>
-          <Route exact path="/" element={<Hero />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/tech-stack" element={<TechStack />} />
-          <Route path="/work" element={<Work />} />
-          <Route path="/resume" element={<Resume />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
-    </>
-  );
+  if (!isMobile) {
+    return (
+      <>
+        <Router>
+          <Header />
+          <Routes>
+            <Route exact path="/" element={<Hero />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/tech-stack" element={<TechStack />} />
+            <Route path="/work" element={<Work />} />
+            <Route path="/resume" element={<Resume />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Router>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <MobileView />
+      </>
+    );
+  }
 }
 
 export default App;
